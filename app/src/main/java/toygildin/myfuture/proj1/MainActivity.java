1cf2a1465d7d0054da4faf289d01bad9b722aa8c
anarchyon <protagonyst@mail.ru>
2023-11-15 17:13:22 +0400

    added nested scroll view

--- app/src/main/java/toygildin/myfuture/proj1/MainActivity.java
+++ app/src/main/java/toygildin/myfuture/proj1/MainActivity.java
@@ -14,27 +14,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_nested_scroll);
 
-        Button button = findViewById(R.id.button);
-        EditText editText1 = findViewById(R.id.editTextNumber1);
-        EditText editText2 = findViewById(R.id.editTextNumber2);
-        TextView textView = findViewById(R.id.textViewResult);
-        RadioButton radioButton1 = findViewById(R.id.radioButton1);
-        RadioButton radioButton2 = findViewById(R.id.radioButton2);
-
-        button.setOnClickListener(v -> {
-            double number1 = editText1.getText().toString().equals("") ?
-                    0 : Integer.parseInt(editText1.getText().toString());
-            double number2 = editText2.getText().toString().equals("") ?
-                    0 : Integer.parseInt(editText2.getText().toString());
-            double result;
-            if (radioButton1.isChecked()) {
-                result = number1 + number2;
-            } else {
-                result = (number1 + number2) / 2;
-            }
-            textView.setText(String.valueOf(result));
-        });
+//        Button button = findViewById(R.id.button);
+//        EditText editText1 = findViewById(R.id.editTextNumber1);
+//        EditText editText2 = findViewById(R.id.editTextNumber2);
+//        TextView textView = findViewById(R.id.textViewResult);
+//        RadioButton radioButton1 = findViewById(R.id.radioButton1);
+//        RadioButton radioButton2 = findViewById(R.id.radioButton2);
+//
+//        button.setOnClickListener(v -> {
+//            double number1 = editText1.getText().toString().equals("") ?
+//                    0 : Integer.parseInt(editText1.getText().toString());
+//            double number2 = editText2.getText().toString().equals("") ?
+//                    0 : Integer.parseInt(editText2.getText().toString());
+//            double result;
+//            if (radioButton1.isChecked()) {
+//                result = number1 + number2;
+//            } else {
+//                result = (number1 + number2) / 2;
+//            }
+//            textView.setText(String.valueOf(result));
+//        });
     }
 }
